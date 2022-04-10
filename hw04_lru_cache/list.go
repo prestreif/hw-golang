bbb8aa10f8bafefbff0d69f60f410307f9483c0c
Зубец Федор <fvzubets1@www.mts.ru>
2022-04-10 15:34:20 +0300

    remove unused code and add coment for fisrs part of hw04_lru_cache

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -38,7 +38,6 @@ func (lst *list) pushFront(itm *ListItem) *ListItem {
 	// Если есть первый элемент, то нужно изменить связи
 	if lst.front != nil {
 		lst.front.Next = itm
-		//itm.Prev = lst.front
 	}
 
 	// Сверху добавляемый элемент и возврат
@@ -49,7 +48,6 @@ func (lst *list) pushFront(itm *ListItem) *ListItem {
 func (lst *list) pushBack(itm *ListItem) *ListItem {
 	// нужно изменить связи
 	lst.back.Prev = itm
-	//itm.Next = lst.back
 
 	// Добавляем элемент и возврат
 	lst.back = itm
@@ -80,33 +78,45 @@ func NewList() List {
 }
 
 func (lst *list) remove(i *ListItem) {
+	// Если есть следующий элемент
 	if i.Next != nil {
+		// то связываем его с предыдущим (или nil)
 		i.Next.Prev = i.Prev
 	} else {
+		// иначе связка списка с предыдущим
 		lst.front = i.Prev
 	}
 
+	// Если есть предыдущий элемент
 	if i.Prev != nil {
+		// то связываем его со следющим
 		i.Prev.Next = i.Next
 	} else {
+		// иначе связка списка со следующим (или nil)
 		lst.back = i.Next
 	}
 
-	i.Prev = nil
-	i.Next = nil
+	// Нужны ли строчки ниже? мне кажется нет
+	// i.Prev = nil
+	// i.Next = nil
 }
 
 func (lst *list) Remove(i *ListItem) {
 	lst.remove(i)
 	lst.length--
-	i = nil
+	// Нужна ли строчка ниже? мне кажется нет
+	//i = nil
 }
 
 func (lst *list) MoveToFront(i *ListItem) {
+	// Если впереди некого то это первый элемент
 	if i.Next == nil {
 		return
 	}
+	// Удалим элемент со старой позиции
 	lst.remove(i)
+	// Свяжем его с первым элементом(обратная связка в нутри pushFront)
 	i.Prev = lst.front
+	// Добавим элемент в верх
 	lst.pushFront(i)
 }
