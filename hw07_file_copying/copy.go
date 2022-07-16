e2ff79d9ea5f9435f491989f74e2ddf4ea9a5baa
Fedor <>
2022-07-16 15:05:51 +0300

    HW07 is complited

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,71 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
+	// This error doesn't need because
+	// if offset equal zero, that app works good
+	// else app will be able to return error ErrOffsetExceedsFileSize
+	ErrUnsupportedFile = errors.New("unsupported file")
+	// This error will be able to return if offset is set and offset more then size file or size file isn't possible counting
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Open In file for read
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer fileFrom.Close()
+
+	// Get info about size file
+	infoFileFrom, err := fileFrom.Stat()
+	if err != nil {
+		return err
+	}
+	sizeFileFrom := infoFileFrom.Size()
+
+	// If size file less then offset
+	if offset > 0 && sizeFileFrom < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// Create out file for write
+	fileOut, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileOut.Close()
+
+	// Set offset file
+	if _, err := fileFrom.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	// Get reader from In file
+	reader := io.Reader(fileFrom)
+
+	// if limit not equal zero that set limit for reader
+	if limit != 0 {
+		reader = io.LimitReader(reader, limit)
+	}
+
+	// create progress bar
+	bar := pb.Full.Start64(sizeFileFrom)
+	defer bar.Finish()
+
+	// wrapp reader into progress bar
+	barReader := bar.NewProxyReader(reader)
+
+	// use base function for copy
+	io.Copy(fileOut, barReader)
+
+	// return nil if everythin ok
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,79 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type testFiles struct {
+	fromPath, toPath, cmpFile string
+	offset, limit             int64
+	expect                    interface{}
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []testFiles{
+		{"", "", "", 0, 0, "open : no such file or directory"},
+		{"testdata/input.txt", "", "", 0, 0, "open : no such file or directory"},
+		{"testdata/input.txt", "testdata/res_out_offset0_limit0.txt", "testdata/out_offset0_limit0.txt", 0, 0, nil},
+		{"testdata/input.txt", "testdata/res_out_offset0_limit0.txt", "testdata/out_offset0_limit0.txt", 7000, 0, ErrOffsetExceedsFileSize.Error()},
+		{"/dev/urandom", "testdata/res_out_urandom_offset0_limit1000.txt", "testdata/out_offset0_limit1000.txt", 1, 1000, ErrOffsetExceedsFileSize.Error()},
+		{"testdata/input.txt", "testdata/res_out_offset0_limit1000.txt", "testdata/out_offset0_limit1000.txt", 0, 1000, nil},
+		{"testdata/input.txt", "testdata/res_out_offset0_limit10000.txt", "testdata/out_offset0_limit10000.txt", 0, 10000, nil},
+		{"testdata/input.txt", "testdata/res_out_offset100_limit1000.txt", "testdata/out_offset100_limit1000.txt", 100, 1000, nil},
+		{"testdata/input.txt", "testdata/res_out_offset6000_limit1000.txt", "testdata/out_offset6000_limit1000.txt", 6000, 1000, nil},
+	}
+	t.Run("Test open files", func(t *testing.T) {
+		for _, val := range tests {
+			if err := Copy(val.fromPath, val.toPath, val.offset, val.limit); err != nil {
+				val_err, _ := val.expect.(string)
+				assert.EqualError(t, err, val_err)
+			} else {
+				assert.Equal(t, val.expect, err)
+			}
+		}
+	})
+
+	t.Run("Test compare size created files", func(t *testing.T) {
+		var err error
+		var cmpFile, toFile *os.File
+		var cmpFileInfo, toFileInfo fs.FileInfo
+
+		for _, val := range tests[5:] {
+			require.FileExists(t, val.cmpFile)
+			require.FileExists(t, val.toPath)
+
+			cmpFile, err = os.Open(val.cmpFile)
+			require.NoError(t, err)
+			cmpFileInfo, err = cmpFile.Stat()
+			require.NoError(t, err)
+
+			toFile, err = os.Open(val.toPath)
+			require.NoError(t, err)
+			toFileInfo, err = toFile.Stat()
+			require.NoError(t, err)
+
+			require.Equal(t, cmpFileInfo.Size(), toFileInfo.Size())
+
+			textToFile, err := io.ReadAll(toFile)
+			require.NoError(t, err)
+
+			textCmpFile, err := io.ReadAll(cmpFile)
+			require.NoError(t, err)
+
+			require.Equal(t, 0, bytes.Compare(textCmpFile, textToFile), val)
+		}
+	})
+
+	t.Run("Clear all created files after tests", func(t *testing.T) {
+		for _, val := range tests {
+			os.Remove(val.toPath)
+		}
+	})
 }
