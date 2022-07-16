b8e89f3c38e9c13f1dc33911c0dddfbf326733b2
Fedor <>
2022-07-16 18:31:38 +0300

    HW8 complited

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		_ = fmt.Errorf("Error: %v\r\n", "need two or more arguments")
+	}
+
+	envs, err := ReadDir(os.Args[1])
+	if err != nil {
+		_ = fmt.Errorf("Error: %v\r\n", err)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], envs))
 }
